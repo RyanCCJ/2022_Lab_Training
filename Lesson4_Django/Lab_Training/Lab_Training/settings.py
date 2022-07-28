bca4062875679c84f7d29b0bded121afde492e3a
Ryan Chung <ryanccj@gmail.com>
2022-07-28 16:18:30 +0800

    edit settings

--- Lesson4_Django/Lab_Training/Lab_Training/settings.py
+++ Lesson4_Django/Lab_Training/Lab_Training/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx#q#8pueq#a+&mcmxjk@n*m(hl36jt8n)yvqe7eb1g=ogwcqd'
+SECRET_KEY = 'XXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
